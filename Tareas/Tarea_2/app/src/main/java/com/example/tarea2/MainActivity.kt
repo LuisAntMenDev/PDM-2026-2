b5f53970e57a5efc3a3bd8c259cf6302c69e42c1
LuisAntMenDev <luisanmen.dev@gmail.com>
2026-03-20 16:15:38 -0600

    fix: Visualizacion de productos en modo landscape

--- Tareas/Tarea_2/app/src/main/java/com/example/tarea2/MainActivity.kt
+++ Tareas/Tarea_2/app/src/main/java/com/example/tarea2/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.tarea2
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.widget.GridLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,6 +16,13 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.login)
         // Vista 2: Productos
         setContentView(R.layout.productos)
+        val orientation = resources.configuration.orientation
+        val grid = findViewById<GridLayout>(R.id.gridProductos)
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            grid.columnCount = 2
+        } else if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            grid.columnCount = 3
+        }
         /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)

b6c5f9bb99bf6690e2608a7f30ff14b023ff6bde
LuisAntMenDev <luisanmen.dev@gmail.com>
2026-03-20 16:02:26 -0600

    feat: Tarea 2

---
+++ Tareas/Tarea_2/app/src/main/java/com/example/tarea2/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.tarea2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        // Vista 1: Login
+        //setContentView(R.layout.login)
+        // Vista 2: Productos
+        setContentView(R.layout.productos)
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }*/
+    }
+}

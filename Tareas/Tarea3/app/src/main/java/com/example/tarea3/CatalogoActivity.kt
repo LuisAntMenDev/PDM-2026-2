8041c858da47b696bcfea653e67376ecb183b895
LuisAntMenDev <luisanmen.dev@gmail.com>
2026-04-21 13:24:21 -0600

    feat: Tarea 3

---
+++ Tareas/Tarea3/app/src/main/java/com/example/tarea3/CatalogoActivity.kt
@@ -0,0 +1,70 @@
+package com.example.tarea3
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
+
+class CatalogoActivity : AppCompatActivity() {
+
+    private lateinit var navigationView: NavigationView
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var topAppBar: MaterialToolbar
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.catalogo)
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        navigationView = findViewById(R.id.navigationView)
+        drawerLayout = findViewById(R.id.drawerLayout)
+        topAppBar = findViewById(R.id.topAppBar)
+        topAppBar.setNavigationOnClickListener {
+            drawerLayout.open()
+        }
+        topAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.action_ajustes -> {
+                    Log.d("HomeActivity", "Se pulsó acción de ajustes")
+                    true
+                }
+
+                R.id.action_info -> {
+                    Log.d("HomeActivity", "Se pulsó acción de más información")
+                    true
+                }
+
+                R.id.action_sesion -> {
+                    Log.d("HomeActivity", "Se pulsó acción de sesión")
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.drawer_catalogo -> {
+                    val intent = Intent(this, CatalogoActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+            menuItem.isChecked = true
+            drawerLayout.close()
+            val mensaje = "Se pulsó la opción: " + menuItem.title;
+            Log.d("HomeActivity", mensaje)
+            true
+
+        }
+    }
+}
